88e51b165752f9f52e0d6fe2a78fddcf61d9f75b
William Lea <wjlea93@gmail.com>
2024-10-24 14:01:10 +0100

    adding signup

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,7 @@ func main() {
 		})
 	})
 
+	// Login
 	router.GET("/login", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "login.html", gin.H{
 			"title": "Login",
@@ -25,6 +26,18 @@ func main() {
 	})
 
 	router.POST("/login", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusSeeOther, "/")
+	})
+
+	// Signup
+	router.GET("/signup", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "signup.html", gin.H{
+			"title": "Signup",
+			"check": uuid.NewString(),
+		})
+	})
+
+	router.POST("/signup", func(ctx *gin.Context) {
 		ctx.Redirect(http.StatusSeeOther, "/")
 	})
 
